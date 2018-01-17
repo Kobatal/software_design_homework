07fad001a553446fb6a0d5e26364b9dd2c855ea1
Kobatal <1131150137@qq.com>
2018-01-17 22:31:58 +0800

    2018.1.17

---
+++ Homework/surface/MainModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace surface
+{
+    class MainModel : INotifyPropertyChanged
+    {
+        public const string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=homework1_16;Integrated Security=True;";
+        public MainModel()
+        {
+            DataContext = new DataClasses1DataContext(ConnectionString);
+        }
+
+        public void Submit()
+        {
+            DataContext.SubmitChanges();
+        }
+        public Table<Person> Persons
+        {
+            get { return DataContext.Person; }
+        }
+        public DataClasses1DataContext DataContext { get; }
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string SelectTarget
+        {
+            get { return _SelectTarget; }
+            set
+            {
+                if (_SelectTarget == value) return; _SelectTarget = value; OnPropertyChanged(nameof(SelectTarget));
+            }
+        }
+        private string _SelectTarget;
+
+        public string CollectedText { get { return _CollectedText; } private set { if (_CollectedText == value) return; _CollectedText = value; OnPropertyChanged(nameof(CollectedText)); } }
+        private string _CollectedText;
+
+        public void GetSelect()
+        {
+            string Result="";
+            List<string> Datalist = new List<string>();
+            var Persons = from r in DataContext.Person select r;
+            foreach (Person aPerson in Persons)
+            {
+                string Person_Data = "Id:" + aPerson.Id + " Name:" + aPerson.Name + " Number:" + aPerson.Number + " Memo:" + aPerson.Memo;
+                Datalist.Add(Person_Data);
+            }
+            string RegexStr = ".*?Memo:"+SelectTarget;
+            for(int i = 0; i < Datalist.Count; i++)
+            {
+                if (Regex.IsMatch(Datalist[i], RegexStr))
+                    Result = Result+Datalist[i]+"\r";
+            }
+            CollectedText = Result;
+        }
+    }
+}
---
+++ Homework/surface/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace surface
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MainModel();
+            this.DataContext = _Model;
+        }
+        private MainModel _Model;
+        private void OnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _Model.Submit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void OnSelect_Click_Exedcuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            _Model.GetSelect();
+        }
+
+        private void OnSelect_Click_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
